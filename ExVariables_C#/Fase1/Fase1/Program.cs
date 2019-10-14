39941b4ee5934ddfa63074aa6a3e332d5ed3ba73
cristian <crpunti.sab@hotmail.com>
2019-10-14 12:18:03 +0200

    Commit Exercici Variables C# and Javascript

---
+++ ExVariables_C#/Fase1/Fase1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FASE 1
+            Console.WriteLine("FASE 1");
+
+
+            String nom = "Cristian\t";
+            String cognom1 = "Puntí\t";
+            String cognom2 = "Sabatés\t";
+
+            int dia = 25;
+            int mes = 9;
+            int any = 1984;
+
+            Console.WriteLine(cognom1 + cognom2 + nom);
+            Console.WriteLine(dia + "/" + mes + "/" + any);
+
+        }
+    }
+}
